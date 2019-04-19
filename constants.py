b147c1549595a9a5b4cd99fe995c95cd1ce49b00
FeryalHisham <fargalaxy24@gmail.com>
2019-04-19 15:45:34 +0200

    Adding Boroughs in constants

--- constants.py
+++ constants.py
@@ -17,3 +17,47 @@ nyc_airports={'JFK':{'min_lng':-73.8352,
     }
     
 }
+
+nyc_boroughs={
+    'manhattan':{
+        'min_lng':-74.0479,
+        'min_lat':40.6829,
+        'max_lng':-73.9067,
+        'max_lat':40.8820
+    },
+    
+    'queens':{
+        'min_lng':-73.9630,
+        'min_lat':40.5431,
+        'max_lng':-73.7004,
+        'max_lat':40.8007
+
+    },
+
+    'brooklyn':{
+        'min_lng':-74.0421,
+        'min_lat':40.5707,
+        'max_lng':-73.8334,
+        'max_lat':40.7395
+
+    },
+
+    'bronx':{
+        'min_lng':-73.9339,
+        'min_lat':40.7855,
+        'max_lng':-73.7654,
+        'max_lat':40.9176
+
+    },
+
+    'staten_island':{
+        'min_lng':-74.2558,
+        'min_lat':40.4960,
+        'max_lng':-74.0522,
+        'max_lat':40.6490
+
+    }
+    
+    
+    
+}
